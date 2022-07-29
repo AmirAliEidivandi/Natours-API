8e01b3b6783ae8b1da43bb320062bee0a97d05b1
AmirAliEidivandi <amirah648887@gmail.com>
2022-07-29 13:39:26 +0430

    create crud

---
+++ app.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+
+// middleware
+app.use(express.json());
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get("/api/v1/tours", (req, res) => {
+    res.status(200).json({
+        status: "success",
+        results: tours.length,
+        data: { tours },
+    });
+});
+
+app.get("/api/v1/tours/:id", (req, res) => {
+    const id = req.params.id * 1;
+    const tour = tours.find((el) => el.id === id);
+
+    if (!tour) {
+        return res.status(404).json({
+            status: "fail",
+            message: "Invalid ID",
+        });
+    }
+
+    res.status(200).json({
+        status: "success",
+        data: {
+            tour,
+        },
+    });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
+
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
+        res.status(201).json({
+            status: "success",
+            data: {
+                tour: newTour,
+            },
+        });
+    });
+});
+
+app.patch("/api/v1/tours/:id", (req, res) => {
+    if (req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: "fail",
+            message: "Invalid ID",
+        });
+    }
+
+    res.status(200).json({
+        status: "success",
+        data: {
+            tour: "<Updated tour here...>",
+        },
+    });
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
