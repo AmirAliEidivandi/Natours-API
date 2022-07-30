0533266e3d58b0723d7ddf52ccd2f740afa6520d
AmirAliEidivandi <amirah648887@gmail.com>
2022-07-30 17:37:22 +0430

    refactor code

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
 const express = require("express");
 const fs = require("fs");
 const app = express();
+const morgan = require("morgan");
 
 // middleware
+app.use(morgan("dev"));
 app.use(express.json());
+app.use((req, res, next) => {
+    console.log("Hello from the middleware");
+    next();
+});
+
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString();
+    next();
+});
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
-app.get("/api/v1/tours", (req, res) => {
+// routes handlers
+const getAllTours = (req, res) => {
+    console.log(req.requestTime);
     res.status(200).json({
         status: "success",
+        requestAt: req.requestTime,
         results: tours.length,
         data: { tours },
     });
-});
+};
 
-app.get("/api/v1/tours/:id", (req, res) => {
+const getTour = (req, res) => {
     const id = req.params.id * 1;
     const tour = tours.find((el) => el.id === id);
 
@@ -32,9 +46,9 @@ app.get("/api/v1/tours/:id", (req, res) => {
             tour,
         },
     });
-});
+};
 
-app.post("/api/v1/tours", (req, res) => {
+const createTour = (req, res) => {
     const newId = tours[tours.length - 1].id + 1;
     const newTour = Object.assign({ id: newId }, req.body);
 
@@ -47,9 +61,9 @@ app.post("/api/v1/tours", (req, res) => {
             },
         });
     });
-});
+};
 
-app.patch("/api/v1/tours/:id", (req, res) => {
+const updateTour = (req, res) => {
     if (req.params.id * 1 > tours.length) {
         return res.status(404).json({
             status: "fail",
@@ -63,7 +77,64 @@ app.patch("/api/v1/tours/:id", (req, res) => {
             tour: "<Updated tour here...>",
         },
     });
-});
+};
 
+const deleteTour = (req, res) => {
+    if (req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: "fail",
+            message: "Invalid ID",
+        });
+    }
+
+    res.status(204).json({
+        status: "success",
+        data: null,
+    });
+};
+
+const getAllUsers = (req, res) => {
+    res.status(500).json({
+        status: "error",
+        message: "This route is not yet defined!!!",
+    });
+};
+
+const getUser = (req, res) => {
+    res.status(500).json({
+        status: "error",
+        message: "This route is not yet defined!!!",
+    });
+};
+
+const createUser = (req, res) => {
+    res.status(500).json({
+        status: "error",
+        message: "This route is not yet defined!!!",
+    });
+};
+
+const updateUser = (req, res) => {
+    res.status(500).json({
+        status: "error",
+        message: "This route is not yet defined!!!",
+    });
+};
+
+const deleteUser = (req, res) => {
+    res.status(500).json({
+        status: "error",
+        message: "This route is not yet defined!!!",
+    });
+};
+
+// routes
+app.route("/api/v1/tours").get(getAllTours).post(createTour);
+app.route("/api/v1/tours/:id").get(getTour).patch(updateTour).delete(deleteTour);
+
+app.route("/api/v1/users").get(getAllUsers).post(createUser);
+app.route("/api/v1/users/:id").get(getUser).patch(updateUser).delete(deleteUser);
+
+// start server
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
