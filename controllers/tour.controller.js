30296f4ce3760725060f2800118b59529040c86c
AmirAliEidivandi <amirah648887@gmail.com>
2022-08-01 14:15:38 +0430

    create checkbody middleware

--- controllers/tour.controller.js
+++ controllers/tour.controller.js
@@ -1,6 +1,27 @@
 const fs = require("fs");
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
 
+const checkId = (req, res, next, val) => {
+    console.log(`Tour id is: ${val}`);
+    if (req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: "fail",
+            message: "Invalid ID",
+        });
+    }
+    next();
+};
+
+const checkBody = (req, res, next) => {
+    if (!req.body.name || !req.body.price) {
+        return res.status(400).json({
+            status: "fail",
+            message: "Missing name or price",
+        });
+    }
+    next();
+};
+
 const getAllTours = (req, res) => {
     console.log(req.requestTime);
     res.status(200).json({
@@ -14,13 +35,6 @@ const getAllTours = (req, res) => {
 const getTour = (req, res) => {
     const id = req.params.id * 1;
     const tour = tours.find((el) => el.id === id);
-
-    if (!tour) {
-        return res.status(404).json({
-            status: "fail",
-            message: "Invalid ID",
-        });
-    }
 
     res.status(200).json({
         status: "success",
@@ -46,13 +60,6 @@ const createTour = (req, res) => {
 };
 
 const updateTour = (req, res) => {
-    if (req.params.id * 1 > tours.length) {
-        return res.status(404).json({
-            status: "fail",
-            message: "Invalid ID",
-        });
-    }
-
     res.status(200).json({
         status: "success",
         data: {
@@ -62,13 +69,6 @@ const updateTour = (req, res) => {
 };
 
 const deleteTour = (req, res) => {
-    if (req.params.id * 1 > tours.length) {
-        return res.status(404).json({
-            status: "fail",
-            message: "Invalid ID",
-        });
-    }
-
     res.status(204).json({
         status: "success",
         data: null,
@@ -81,4 +81,6 @@ module.exports = {
     createTour,
     updateTour,
     deleteTour,
+    checkId,
+    checkBody,
 };
--- routes/tour.routes.js
+++ routes/tour.routes.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
-const { getAllTours, getTour, createTour, updateTour, deleteTour } = require("../controllers/tour.controller");
+const { getAllTours, getTour, createTour, updateTour, deleteTour, checkId, checkBody } = require("../controllers/tour.controller");
 
-router.route("/").get(getAllTours).post(createTour);
+router.param("id", checkId);
+
+router.route("/").get(getAllTours).post(checkBody, createTour);
 router.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
