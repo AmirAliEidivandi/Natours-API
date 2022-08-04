dece0e804be097fc0ff7c16341872c05a3a6a861
AmirAliEidivandi <amirah648887@gmail.com>
2022-08-04 23:28:21 +0430

    create filtering

--- controllers/tour.controller.js
+++ controllers/tour.controller.js
@@ -2,7 +2,19 @@ const Tour = require("../models/tour.model");
 
 const getAllTours = async (req, res) => {
     try {
-        const tours = await Tour.find({});
+        // build query
+        const queryObj = { ...req.query };
+        const excludedFields = ["page", "sort", "limit", "fields"];
+        excludedFields.forEach((el) => delete queryObj[el]);
+
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+        const query = Tour.find(JSON.parse(queryStr));
+
+        // excute query
+        const tours = await query;
+        // send response
         res.status(200).json({
             status: "success",
             results: tours.length,
