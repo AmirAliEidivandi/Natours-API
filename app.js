9d573f7d71490b7220f36bb6626ddbd734492c70
AmirAliEidivandi <amirah648887@gmail.com>
2022-08-08 14:15:27 +0430

    create error

--- app.js
+++ app.js
@@ -11,20 +11,29 @@ if (process.env.NODE_ENV === "development") {
 }
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(`${__dirname}/public`));
-
-app.use((req, res, next) => {
-    console.log("Hello from the middleware");
-    next();
-});
-
-app.use((req, res, next) => {
-    req.requestTime = new Date().toISOString();
-    next();
-});
 
 // routes
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
+
+app.all("*", (req, res, next) => {
+    const err = new Error(`Can't find ${req.originalUrl} on this server`);
+    err.status = "fail";
+    err.statusCode = 404;
+
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || "error";
+
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+    });
+});
 
 module.exports = app;
