4ec10399e550b08bbae1551ae6601d857b7115d9
AmirAliEidivandi <amirah648887@gmail.com>
2022-08-10 23:59:43 +0430

    add model

---
+++ controllers/auth.controller.js
@@ -0,0 +1,13 @@
+const User = require("../models/user.model");
+const catchAsync = require("../utils/catch.async");
+
+exports.signup = catchAsync(async (req, res, next) => {
+    const newUser = await User.create(req.body);
+
+    res.status(201).json({
+        status: "success",
+        data: {
+            user: newUser,
+        },
+    });
+});
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,8 @@
 const router = require("express").Router();
 const { getAllUsers, getUser, createUser, deleteUser, updateUser } = require("../controllers/user.controller");
+const { signup } = require("../controllers/auth.controller");
+
+router.post("/signup", signup);
 
 router.route("/").get(getAllUsers).post(createUser);
 router.route("/:id").get(getUser).patch(updateUser).delete(deleteUser);
