57f06dd7f90673fa304ec0bf42d61917012dd8fa
AmirAliEidivandi <amirah648887@gmail.com>
2022-08-23 14:26:04 +0430

    change route

--- routes/tour.routes.js
+++ routes/tour.routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { getAllTours, getTour, createTour, updateTour, deleteTour, aliasTopTours, getTourStats, getMonthlyPlan } = require("../controllers/tour.controller");
-const { protect } = require('../controllers/auth.controller');
+const { protect } = require("../controllers/auth.controller");
 
 router.route("/top-5-cheap").get(aliasTopTours, getAllTours);
 router.route("/tour-stats").get(getTourStats);
